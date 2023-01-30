3033fe0f4019dd8d73df3377b6fb1b03bff8163c
Diallo boubacar <diallob456@gmail.com>
2023-01-30 20:38:11 +0100

    Exercice_XP_GOLD

---
+++ Exercice2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+ 
+<?php 
+$str="Hello World!";
+
+echo str_word_count($str);
+
+ ?>
+
+</body>
+</html>
